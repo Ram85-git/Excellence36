ea79850b4d309f16eb72c2f9fa51b14642e97aa8
Ram <126403725+Ram85-git@users.noreply.github.com>
2023-06-10 06:56:49 -0400

    Form added

---
+++ card37/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  
+  <HelloWorld />
+  <SimpleForm/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import SimpleForm from './components/SimpleForm.vue';
+import SimpleForm from './components/SimpleForm.vue';
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    SimpleForm,
+    SimpleForm
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
